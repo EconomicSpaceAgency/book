6d04201eb5144048f690aed463b3a342af361088
polux0 <afidelo@protonmail.com>
2023-11-22 00:00:06 +0100

    feat: gdpr consent;

--- validation/validateOrders.js
+++ validation/validateOrders.js
@@ -1,20 +1,32 @@
+import { getConsentGiven } from "./getConsentGiven";
 function validateOrders() {
     let deliveryName = document.getElementById("name");
     let deliveryMailing = document.getElementById("mailingAddress");
     let deliveryPhoneNumber = document.getElementById("phoneNumber");
+    let consentGiven = getConsentGiven();
 
     let deliveryError = document.getElementById("detailsError");
+    let validOrder = true;
+
+    if(!consentGiven){
+        deliveryError.style.display = "block";
+        validOrder = false;
+    }
 
     if (!deliveryName.value.trim()) {
         deliveryError.style.display = "block";
+        validOrder = false;
     }
     
     if (!deliveryMailing.value.trim()) {
         deliveryError.style.display = "block";
+        validOrder = false;
     }
     
     if (!deliveryPhoneNumber.value.trim()) {
         deliveryError.style.display = "block";
+        validOrder = false;
     }
+    return validOrder;
 }
 export {validateOrders}
